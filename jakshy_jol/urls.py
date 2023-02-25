509fa5328f3f73c2c284747115813a6d48906f9c
zhibek13 <jibek1309.jk@gmail.com>
2023-02-25 18:12:34 +0600

    create view

--- jakshy_jol/urls.py
+++ jakshy_jol/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 
 urlpatterns = [
-    path('news/', views.NewsListCreate.as_view())
+    path('news/', views.NewsList.as_view())
 ]
--- jakshy_jol/views.py
+++ jakshy_jol/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListAPIView
 
 from .models import News
 from .serializers import NewsSerializer
 
 
-class NewsListCreate(ListCreateAPIView):
+class NewsList(ListAPIView):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
 
