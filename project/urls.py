c30c1c182f14cf8f8a2c10c4bf2c4b3edf95ba9b
zhibek13 <jibek1309.jk@gmail.com>
2023-02-25 18:04:08 +0600

    url

--- project/urls.py
+++ project/urls.py
@@ -21,5 +21,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('jakshy_jol.urls')),
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
